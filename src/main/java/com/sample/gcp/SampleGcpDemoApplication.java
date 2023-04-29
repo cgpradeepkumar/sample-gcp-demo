8ca9dcf00351a88b6ee622e26c2375d52ee1ead2
Pradeepkumar <cgpradeepkumar@gmail.com>
2023-04-29 16:42:37 +0530

    initial

---
+++ src/main/java/com/sample/gcp/SampleGcpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sample.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGcpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGcpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/gcp/SampleGcpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.gcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGcpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
